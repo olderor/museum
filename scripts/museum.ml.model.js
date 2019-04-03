38bc7427d7757ded699bed4a1e47c88842e1234e
Bohdan Yevchenko <98body@gmail.com>
2019-04-03 22:53:40 +0300

    Update museum.ml.model.js

--- scripts/museum.ml.model.js
+++ scripts/museum.ml.model.js
@@ -8,7 +8,7 @@ museum.ml.model = (function() {
        'International', 'Jazz', 'Metal', 'Reggae', 'Rock'];
     
     async function loadModel() {
-        model = await tf.loadLayersModel('scripts/model.js');
+        model = await tf.loadLayersModel('scripts/ml_model/model.json');
     }
     
     function predict(features) {
